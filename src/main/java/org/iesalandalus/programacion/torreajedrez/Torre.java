5d9e845fe4aa8ffcc6c6b37abfb0dcb6cecda6f1
José Francisco Hernández Allen <darthvladeroficial@gmail.com>
2022-05-30 22:49:04 +0200

    JFHA: 12.- Generamos Getters and Setters.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -2,4 +2,42 @@ package org.iesalandalus.programacion.torreajedrez;
 
 public class Torre {
 
+//CREAMOS LOS ATRIBUTOS
+	private Color color;
+	private Posicion posicion;
+	
+//GENERAMOS GETTERS AND SETTERS
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	private void setColor(Color color) {
+		if (color==null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		} else {
+			this.color = color;
+		}
+	}
+	/**
+	 * @return the posicion
+	 */
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	/**
+	 * @param posicion the posicion to set
+	 */
+	private void setPosicion(Posicion posicion) {
+		if (posicion==null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		this.posicion = posicion;
+	}
+	
+	
 }
