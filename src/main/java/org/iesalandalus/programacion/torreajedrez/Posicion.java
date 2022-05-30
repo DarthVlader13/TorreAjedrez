6b8b1a332c288ef3aa5809900aa7e0cf713f5f56
José Francisco Hernández Allen <darthvladeroficial@gmail.com>
2022-05-30 22:11:12 +0200

    JFHA: 6.- Creamos getters and setters con los valores correctos.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -5,4 +5,39 @@ public class Posicion {
 //CREAMOS LOS ATRIBUTOS FILA Y COLUMNA
 	private int fila;
 	private char columna;
+	/**
+	 * @return the fila
+	 */
+	public int getFila() {
+		return fila;
+	}
+	/**
+	 * @param fila the fila to set
+	 */
+	public void setFila(int fila) {
+		if (fila <1 || fila >8) {
+			throw new IllegalArgumentException ("ERROR: Fila no válida.");
+		}else {
+			this.fila = fila;
+		}
+	}
+	/**
+	 * @return the columna
+	 */
+	public char getColumna() {
+		return columna;
+	}
+	/**
+	 * @param columna the columna to set
+	 */
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException ("ERROR: Columna no válida.");
+		}else {	
+			this.columna = columna;	
+		}
+	}
+	
+//GENERAMOS GETTERS AND SETTERS
+	
 }
