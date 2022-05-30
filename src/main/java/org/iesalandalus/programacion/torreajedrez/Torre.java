1d5695355dea7cdea4eb6ab06dc3f2b9e3ae7a34
José Francisco Hernández Allen <darthvladeroficial@gmail.com>
2022-05-30 22:54:14 +0200

    JFHA: 13/14/15.- Creamos:
    13.- Constructor por defecto
    14.- Constructor con el parámetro Color
    15.- Constructor con los parámetros Color y Columna

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -5,7 +5,44 @@ public class Torre {
 //CREAMOS LOS ATRIBUTOS
 	private Color color;
 	private Posicion posicion;
-	
+
+//CREAMOS CONSTRUCTOR POR DEFECTO
+	public Torre() {
+		setColor(Color.NEGRO);
+		setPosicion(new Posicion(8, 'h'));
+	}
+
+//CREAMOS CONSTRUCTOR CON EL PARAMETRO COLOR
+	public Torre(Color color) {
+		setColor(color);
+		if (color.equals(null)) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		} else {
+			if (color.equals(Color.BLANCO)) {
+				setPosicion(new Posicion(1, 'h'));
+			} else {
+				setPosicion(new Posicion(8, 'h'));
+			}
+		}
+	}
+
+//CREAMOS CONSTRUCTOR CON LOS PARAMTROS COLOR Y COLUMNA
+	public Torre(Color color, char columna) {
+		setColor(color);
+		if (columna == 'a' || columna == 'h') {
+			if (color.equals(Color.BLANCO)) {
+				setPosicion(new Posicion(1, columna));
+			} else {
+				setPosicion(new Posicion(8, columna));
+			}
+		} else if (color.equals(null)) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		} else {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+
+	}
+
 //GENERAMOS GETTERS AND SETTERS
 	/**
 	 * @return the color
@@ -13,31 +50,33 @@ public class Torre {
 	public Color getColor() {
 		return color;
 	}
+
 	/**
 	 * @param color the color to set
 	 */
 	private void setColor(Color color) {
-		if (color==null) {
+		if (color == null) {
 			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
 		} else {
 			this.color = color;
 		}
 	}
+
 	/**
 	 * @return the posicion
 	 */
 	public Posicion getPosicion() {
 		return posicion;
 	}
+
 	/**
 	 * @param posicion the posicion to set
 	 */
 	private void setPosicion(Posicion posicion) {
-		if (posicion==null) {
+		if (posicion == null) {
 			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
 		}
 		this.posicion = posicion;
 	}
-	
-	
+
 }
